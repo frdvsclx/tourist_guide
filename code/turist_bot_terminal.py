ecdcf354302e356fe2a2ac67308443949585e325
Firdevs Çolak <firdevscolak81@gmail.com>
2025-07-23 22:10:00 +0300

    happy lama

--- code/turist_bot_terminal.py
+++ code/turist_bot_terminal.py
@@ -30,16 +30,21 @@ happy_lama = ChatOllama(model="llama3.2:3b")
 
 memory = ConversationBufferMemory(return_messages=True) #mesajlar formatlı döner
 
-print("selam ben turizm rehberi ;) ...")
+print("hi im lama, here to help ;) ...")
 
 while True:
     user_input = input("siz:")
     
-    if user_input.lower() =="quit"
+    if user_input.lower() == "quit":
         print("lama is sad, by")
-        break
+        break 
     
+    memory.chat_memory.add_user_message(user_input)
     
+    messages = [SystemMessage(content="you are a tourist guide..")] + memory.load_memory_variables({})["history"] 
+    + [HumanMessage(content=user_input)]
 
-
+    response = happy_lama(messages) #modelden yanit alma
+    memory.chat_memory.add_ai_message(response.content)
+    print(f"guide: {response.content}")
 
